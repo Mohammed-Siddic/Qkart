b4e2c0548dd0fe7e76a697c7faa6fb089b90beab
Mohammed Siddic <shawnsid10@gmail.com>
2024-01-20 14:29:03 +0530

    first commit

---
+++ src/models/user.model.js
@@ -0,0 +1,108 @@
+const mongoose = require("mongoose");
+// NOTE - "validator" external library and not the custom middleware at src/middlewares/validate.js
+const validator = require("validator");
+const config = require("../config/config");
+const bcrypt = require('bcryptjs');
+
+// TODO: CRIO_TASK_MODULE_UNDERSTANDING_BASICS - Complete userSchema, a Mongoose schema for "users" collection
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      lowercase: true,
+      validate(value) {
+        if(!validator.isEmail(value)){
+          throw new Error("Invalid Email");
+        }
+      }
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 8,
+      validate(value) {
+        if (!value.match(/\d/) || !value.match(/[a-zA-Z]/)) {
+          throw new Error(
+            "Password must contain at least one letter and one number"
+          );
+        }
+      },
+    },
+    walletMoney: {
+      type: Number,
+      required: true,
+      default: config.default_wallet_money,
+    },
+    address: {
+      type: String,
+      default: config.default_address,
+    },
+  },
+  // Create createdAt and updatedAt fields automatically
+  {
+    timestamps: true,
+  }
+);
+
+// TODO: CRIO_TASK_MODULE_UNDERSTANDING_BASICS - Implement the isEmailTaken() static method
+/**
+ * Check if email is taken
+ * @param {string} email - The user's email
+ * @returns {Promise<boolean>}
+ */
+userSchema.statics.isEmailTaken = async function (email) {
+  const user = await this.findOne({email:email});
+  return user;
+};
+
+userSchema.pre('save', function (next) {
+  const salt = bcrypt.genSaltSync();
+  this.password = bcrypt.hashSync(this.password, salt);
+next();
+})
+
+userSchema.methods.isPasswordMatch = async function (password){
+return bcrypt.compare(password, this.password);
+}
+
+
+// TODO: CRIO_TASK_MODULE_UNDERSTANDING_BASICS
+/*
+ * Create a Mongoose model out of userSchema and export the model as "User"
+ * Note: The model should be accessible in a different module when imported like below
+ * const User = require("<user.model file path>").User;
+ */
+/**
+ * @typedef User
+ */
+
+// module.exports = { userSchema};
+
+/**
+ * Check if user have set an address other than the default address
+ * - should return true if user has set an address other than default address
+ * - should return false if user's address is the default address
+ * ok
+ *
+ * @returns {Promise<boolean>}
+ */
+
+ userSchema.methods.hasSetNonDefaultAddress = async function () {
+  const user = this;
+   return user.address !== config.default_address;
+   //MAKE SURE YOU CHANGE THIS EQAULITY OPERATOR TO NON EQUALITY OPERATOR TO PASS THE TEST CASES
+};
+
+
+
+const User = mongoose.model("User", userSchema);
+module.exports = {User} ;
